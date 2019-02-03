ffeed7f282e12084c05094f61b0bbd8853011314
Marius Metzger <marius@crushedpixel.net>
2019-02-03 16:13:21 +0100

    Add support for certain Intel GPUs

--- assets/vert.glsl
+++ assets/vert.glsl
@@ -17,6 +17,6 @@
 #version 120
 uniform vec2 iResolution;
 void main() {
-  vec2 pos = (gl_Vertex.xy / iResolution) * 2.0 - vec2(1, 1);
+  vec2 pos = (gl_Vertex.xy / iResolution) * 2.0 - vec2(1.0, 1.0);
   gl_Position = vec4(pos, 0.0, 1.0);
 }
