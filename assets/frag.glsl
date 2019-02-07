8690f6e6700170c5049f4a1088c46852eced2caa
hackerpoet <hackerpoet@users.noreply.github.com>
2019-02-07 01:53:50 -0800

    Small optimization.

--- assets/frag.glsl
+++ assets/frag.glsl
@@ -201,12 +201,12 @@ vec4 col_scene(vec4 p) {
 
 //A faster formula to find the gradient/normal direction of the DE(the w component is the average DE)
 //credit to http://www.iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
-vec4 calcGrad(vec4 p, float dx) {
+vec3 calcGrad(vec4 p, float dx) {
 	const vec3 k = vec3(1,-1,0);
-	return (k.xyyx*DE(p + k.xyyz*dx) +
-		k.yyxx*DE(p + k.yyxz*dx) +
-		k.yxyx*DE(p + k.yxyz*dx) +
-		k.xxxx*DE(p + k.xxxz*dx)) / vec4(4*dx,4*dx,4*dx,4);
+	return (k.xyy*DE(p + k.xyyz*dx) +
+		k.yyx*DE(p + k.yyxz*dx) +
+		k.yxy*DE(p + k.yxyz*dx) +
+		k.xxx*DE(p + k.xxxz*dx)) / vec3(4*dx,4*dx,4*dx);
 }
 
 //find the average color of the fractal in a radius dx in plane s1-s2
@@ -256,8 +256,7 @@ vec4 scene(inout vec4 p, inout vec4 ray, float vignette) {
 	float min_dist = max(FOVperPixel*td, MIN_DIST);
 	if (d < min_dist) {
 		//Get the surface normal
-		vec4 grad = calcGrad(p,min_dist*0.5);
-		vec3 n = normalize(grad.xyz);
+		vec3 n = normalize(calcGrad(p,min_dist*0.5));
 		
 		//find closest surface point, without this we get weird coloring artifacts
 		p.xyz -= n*d;
