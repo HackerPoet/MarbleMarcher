a8ff1693b459de98739fa1b76d6d890747e0a129
Bryce Wilson <bryce@thenetworknerds.ca>
2019-02-24 11:57:53 -0800

    A few fixes

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,9 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.If not, see <http://www.gnu.org/licenses/>.
 */
+#ifdef __MINGW32__
 #include "fixthings.c"
+#endif
 #include "Scene.h"
 #include "Level.h"
 #include "Overlays.h"
