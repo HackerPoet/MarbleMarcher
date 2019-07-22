a46eb0edc4378dc18e63d01415e4345b10e942e5
Moroz Michael <47035925+MichaelMoroz@users.noreply.github.com>
2019-07-22 11:14:44 +0300

    opengl

---
+++ src/Shaders.cpp
@@ -0,0 +1,98 @@
+#include "Shaders.h"
+
+void ComputeShader::LoadShader(const std::string file_path)
+{
+		// Create the shaders
+		GLuint VertexShaderID = glCreateShader(GL_COMPUTE_SHADER);
+		GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// Read the Vertex Shader code from the file
+		std::string VertexShaderCode;
+		std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+		if (VertexShaderStream.is_open())
+		{
+			std::string Line = "";
+			while (getline(VertexShaderStream, Line))
+				VertexShaderCode += "\n" + Line;
+			VertexShaderStream.close();
+		}
+		else
+		{
+			ERROR_MSG(("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n" + (string)vertex_file_path).c_str());
+			getchar();
+		}
+
+		// Read the Fragment Shader code from the file
+		std::string FragmentShaderCode;
+		std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+		if (FragmentShaderStream.is_open())
+		{
+			std::string Line = "";
+			while (getline(FragmentShaderStream, Line))
+				FragmentShaderCode += "\n" + Line;
+			FragmentShaderStream.close();
+		}
+
+		GLint Result = GL_FALSE;
+		int InfoLogLength;
+
+
+		// Compile Vertex Shader
+		LOGGERER("Compiling shader: " + (string)vertex_file_path);
+		char const * VertexSourcePointer = VertexShaderCode.c_str();
+		glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
+		glCompileShader(VertexShaderID);
+
+		// Check Vertex Shader
+		glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0)
+		{
+			std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+			LOGGERER(&VertexShaderErrorMessage[0]);
+		}
+
+
+		// Compile Fragment Shader
+		LOGGERER("Compiling shader:" + (string)fragment_file_path);
+		char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+		glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
+		glCompileShader(FragmentShaderID);
+
+		// Check Fragment Shader
+		glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+		glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0)
+		{
+			std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+			glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+			LOGGERER(&FragmentShaderErrorMessage[0]);
+		}
+
+
+
+		// Link the program
+		LOGGERER("Linking program!");
+		ProgramID = glCreateProgram();
+		glAttachShader(ProgramID, VertexShaderID);
+		glAttachShader(ProgramID, FragmentShaderID);
+		glLinkProgram(ProgramID);
+
+		// Check the program
+		glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
+		glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+		if (InfoLogLength > 0)
+		{
+			std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+			glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+			LOGGERER(&ProgramErrorMessage[0]);
+		}
+
+
+		glDetachShader(ProgramID, VertexShaderID);
+		glDetachShader(ProgramID, FragmentShaderID);
+
+		glDeleteShader(VertexShaderID);
+		glDeleteShader(FragmentShaderID);
+}
--- src/Shaders.h
+++ src/Shaders.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <string>
+#include <GL/glew.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 
 class ComputeShader
 {
@@ -10,12 +14,12 @@ public:
 
 	void LoadShader(const std::string file_path);
 
-	void setUniform(string name, float X, float Y);
-	void setUniform(string name, float X, float Y, float Z);
-	void setUniform(string name, float X);
-	void setUniform(string name, int X);
-	void setUniform(string name, Eigen::Matrix3f X, bool transpose);
-	void setUniform(string name, Eigen::Vector3f X);
+	void setUniform(std::string name, float X, float Y);
+	void setUniform(std::string name, float X, float Y, float Z);
+	void setUniform(std::string name, float X);
+	void setUniform(std::string name, int X);
+	void setUniform(std::string name, Eigen::Matrix3f X, bool transpose);
+	void setUniform(std::string name, Eigen::Vector3f X);
 
 
 	GLuint getNativeHandle()
