da5407ffc0df1088fdf4b4571e3b2f8d7e02dd37
Anders Christiansen Sørby <anders@sorby.xyz>
2021-06-14 00:11:50 +0200

    Add run and package build

--- flake.nix
+++ flake.nix
@@ -9,9 +9,27 @@
       (system:
         let 
           pkgs = nixpkgs.legacyPackages.${system};
+          name = "MarbleMatcher";
           inputs = with pkgs; [ eigen sfml cmake gnumake libGL ];
+          package = pkgs.stdenv.mkDerivation {
+            name = name;
+            src = self;
+            buildInputs = inputs;
+            buildPhase = ''
+              cmake -DCMAKE_CXX_FLAGS="-I/usr/local/include" .
+              cmake --build .
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp MarbleMarcher $out/
+            '';
+          };
         in
         {
+          defaultApp = {
+            type = "app";
+            program = "${package}/MarbleMarcher";
+          };
           devShell = pkgs.mkShell { buildInputs = inputs; };
         }
       );
